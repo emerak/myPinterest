d8a12f186bc9108bab29a5bb0e2fdb388d2457a2
Alejandra Cernas <alejandra.cernas@tangosource.com>
2013-09-18 16:56:17 -0500

    Added method to create user from twitter auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,28 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :async,
-         :token_authenticatable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :token_authenticatable, :omniauthable, :omniauth_providers => [:twitter]
 
-  after_save :send_welcome_email
+  extend OmniauthCallbacks
+  before_save :ensure_authentication_token
+  after_save  :send_welcome_email
   has_many :boards
 
+  def find_or_create_for_twitter(response)
+    data = response['extra']['raw_info']
+    if user = User.find_by_twitter_id(data["id"].to_s)
+      user
+    else # Create a user with a stub password.
+      user = User.new(:email => "twitter+#{data["id"]}@example.com",
+                      :password => Devise.friendly_token[0,20])
+                      user.twitter_id = data["id"]
+                      user.twitter_screen_name = data["screen_name"]
+                      user.twitter_display_name = data["display_name"]
+                      user.confirm!
+                      user
+    end
+  end
   private
 
   def send_welcome_email
